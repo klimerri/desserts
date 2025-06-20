07f8de7333e9c21b4ddaddc501b828758a068986
limerri <kovaleva.a2006@yandex.ru>
2025-06-20 18:14:37 +0400

    counter is working

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+function changeDataCount () {
+
+}
+
 const addButtons = document.querySelectorAll('.button__add');
 const activeButtons = document.querySelectorAll('.button__active');
-
-console.log(addButtons);
+const incrementButtons = document.querySelectorAll('.increment');
+const decrementButtons = document.querySelectorAll('.decrement'); 
 
 addButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -14,7 +18,41 @@ addButtons.forEach(button => {
 
             if (dataButton === dataSearchButton) {
                 button.style.display = 'flex';
+
+                const span = button.querySelector('span');
+
+                dataCount = button.getAttribute("data-count");
+                newData = +dataCount + 1;
+                span.textContent = newData;
+
+                button.setAttribute("data-count", newData);
             }
         })
+    })
+})
+
+incrementButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        parentBlock = button.parentNode;
+        const span = parentBlock.querySelector('span');
+
+        dataCount = parentBlock.getAttribute("data-count");
+        newData = +dataCount + 1;
+        span.textContent = newData;
+
+        parentBlock.setAttribute("data-count", newData);
+    })
+})
+
+decrementButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        parentBlock = button.parentNode;
+        const span = parentBlock.querySelector('span');
+
+        dataCount = parentBlock.getAttribute("data-count");
+        newData = +dataCount - 1;
+        span.textContent = newData;
+
+        parentBlock.setAttribute("data-count", newData);
     })
 })
